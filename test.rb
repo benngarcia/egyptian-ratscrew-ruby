da55263c4ad70cf747382b33a0eabee9203770c2
benngarcia <beng4606@gmail.com>
2022-03-22 04:43:14 -0700

    break it for now

--- test.rb
+++ test.rb
@@ -2,17 +2,5 @@ require 'pry'
 
 if __FILE__ == $0
   Dir[File.join(__dir__, 'lib', '**', '*.rb')].sort.each { |file| load file }
-  t1 = Time.now
-  count = {
-    Strategies::REFLEXIVE => 0,
-    Strategies::QUALITATIVE::ALL_FACES => 0
-  }
-  10_000.times do
-    players = [Player.new(Strategies::QUALITATIVE::ALL_FACES), Player.new(Strategies::REFLEXIVE), Player.new(Strategies::REFLEXIVE), Player.new(Strategies::REFLEXIVE)]
-    winner = Game.new(players, GameTypes::CONTROLLED).run_game!
-    count[winner] += 1
-  end
-  t2 = Time.now
-  puts t2 - t1
-  puts count
+
 end
